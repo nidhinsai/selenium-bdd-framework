9d67ef6817b9fcec063ebdcce1e6dc00a12ddadf
Nidhin Raj Panthal Padikkal <nidhinsaikripa@gmail.com>
2026-04-12 03:29:19 -0400

    Add src/main/java/com/nidhinsai/framework/utils/WaitUtil.java

---
+++ src/main/java/com/nidhinsai/framework/utils/WaitUtil.java
@@ -0,0 +1,25 @@
+package com.nidhinsai.framework.utils;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WaitUtil {
+    private final WebDriverWait wait;
+
+    public WaitUtil(WebDriver driver) {
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(
+                Integer.parseInt(ConfigReader.get("app.timeout.explicit", "20"))));
+    }
+
+    public WebElement visible(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public WebElement clickable(By locator) {
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+}
